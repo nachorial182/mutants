7bc5223c4b8fa7e88f09400fee72c3274c4acbbb
Ignacio Rial <ignacio.rial@favordelivery.com>
2025-07-09 01:05:52 -0300

    Add validation to allow only A, T, C, and G characters in DNA input

--- src/main/java/com/example/mutants/dtos/DnaRequest.java
+++ src/main/java/com/example/mutants/dtos/DnaRequest.java
@@ -1,6 +1,7 @@
 package com.example.mutants.dtos;
 
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -14,6 +15,7 @@ public class DnaRequest {
 
     @NotEmpty(message = "DNA sequence is required")
     @Size(min = 4, message = "DNA must contain at least 4 rows")
-    private List<String> dna;
+    private List<
+            @Pattern(regexp = "^[ATCG]+$", message = "Each DNA string must contain only the characters A, T, C, or G")
+                    String> dna;
 }
-
